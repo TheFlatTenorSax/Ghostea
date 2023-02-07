ba85f506b8c63e8613cab02f1e50d0b9856cb37c
TheFlatTenorSax <rtc907335@gmail.com>
2023-02-06 21:17:07 -0800

    Initial Commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -40,7 +40,7 @@ public class Robot extends TimedRobot {
     right1 = new TalonFX(4);
     right2 = new TalonFX(2);
     left1 = new TalonFX(3);
-    left2 = new TalonFX(1);
+    left2 = new TalonFX(1); 
     gamer = new XboxController(0);
     left2.follow(left1);
     right2.follow(right1);
